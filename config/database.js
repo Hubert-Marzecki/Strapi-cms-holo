51f04f497eb05a0226480762a4b288948aa126f9
Hubert Marzecki <hubert.marzecki@gmail.com>
2020-07-26 13:25:26 +0200

    configured

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', ''),
-        password: env('DATABASE_PASSWORD', ''),
+        host: env('DATABASE_HOST', 'ec2-50-37-231-192.compute-2.amazonaws.com'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'd516fp1u21ph7b'),
+        username: env('DATABASE_USERNAME', 'ebitxebvixeeqd'),
+        password: env('DATABASE_PASSWORD', 'dc59b16dedb3a1eef84d4999a0be041bd419c474cd4a0973efc7c9339afb4baf'),
       },
       options: {
         ssl: false,
